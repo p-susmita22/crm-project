7ecb9834744fe945937d2c2ebfd5d13ce3d29766
p-susmita22 <susmita@example.com>
2026-09-24 17:11:21 +0530

    Fix syntax error in whatsappController.js

--- backend/controllers/whatsappController.js
+++ backend/controllers/whatsappController.js
@@ -422,6 +422,7 @@ export const receiveMessage = async (req, res) => {
                       });
                     }
                 }
+                }
             }
         }
       } else if (
